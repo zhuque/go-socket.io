58cd592bd2dc9dbee6b2565eef734e1290512d1c
phoenix <ahmmzxf@163.com>
2023-10-29 23:55:48 +0800

    add transport

--- client.go
+++ client.go
@@ -9,6 +9,7 @@ import (
 	"github.com/googollee/go-socket.io/engineio"
 	"github.com/googollee/go-socket.io/engineio/transport"
 	"github.com/googollee/go-socket.io/engineio/transport/polling"
+	"github.com/googollee/go-socket.io/engineio/transport/websocket"
 	"github.com/googollee/go-socket.io/logger"
 	"github.com/googollee/go-socket.io/parser"
 )
@@ -65,7 +66,7 @@ func fmtNS(ns string) string {
 
 func (c *Client) Connect() error {
 	dialer := engineio.Dialer{
-		Transports: []transport.Transport{polling.Default},
+		Transports: []transport.Transport{polling.Default, websocket.Default},
 	}
 
 	enginioCon, err := dialer.Dial(c.url, nil)
